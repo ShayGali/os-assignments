4d768a908543345df4ace0fcc7ff2c35a565648c
shayg <shaygali10@gmail.com>
2024-05-29 17:42:12 +0300

    very nice q3

--- ex2/q3/mynetcat.c
+++ ex2/q3/mynetcat.c
@@ -6,79 +6,43 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <sys/un.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
-/**
- * Run the program with the given arguments
- */
-void run_program(char *args_as_string) {
-    // tokenize the string - split by space
-    char *token = strtok(args_as_string, " ");
+// start the input and output file descriptors with the default values
+// and will change them if needed
+int input_fd = STDIN_FILENO;
+int output_fd = STDOUT_FILENO;
 
-    if (token == NULL) {
-        fprintf(stderr, "No arguments provided\n");
-        exit(EXIT_FAILURE);
+void cleanup_and_exit(int exit_code) {
+    if (input_fd != STDIN_FILENO) {
+        close(input_fd);
     }
-    // create an array of strings to store the arguments
-    char **args = (char **)malloc(sizeof(char *));
-    int n = 0;          // number of arguments
-    args[n++] = token;  // add the first argument
-
-    // get the rest of the arguments
-    while (token != NULL) {
-        token = strtok(NULL, " ");                                    // get the next token (NULL - take the next token from the previous string)
-        char **t = (char **)realloc(args, (n + 1) * sizeof(char *));  // allocate memory for the new argument
-        // check if the memory allocation failed
-        if (t == NULL) {
-            fprintf(stderr, "Memory allocation failed\n");
-            free(args);
-            exit(1);
-        }
-        args = t;
-        args[n++] = token;  // add the new argument and increment the number of arguments
+    if (output_fd != STDOUT_FILENO && output_fd != input_fd) {
+        close(output_fd);
     }
-    // fork and execute the program
-    int fd = fork();
-    if (fd < 0) {  // fork failed
-        fprintf(stderr, "Fork failed\n");
-        exit(EXIT_FAILURE);
-    }
-
-    if (fd == 0) {  // child process
-        execvp(args[0], args);
-        fprintf(stderr, "Exec failed\n");
-        free(args);
-        exit(EXIT_FAILURE);
-    } else {
-        wait(NULL);  // wait for the child process to finish
-        // free the memory
-        free(args);
-        fflush(stdout);
-    }
+    exit(exit_code);
 }
 
 /**
- * Open a TCP server to listen to the given port
- * @param i_value the port to listen to (in format "TCPS<port>")
- * @param input_fd the file descriptor to store the new socket (return value)
+ * Open a TCP server to listen to the given port and accept the connection
+ * @param port the port to listen to
+ * @return the file descriptor of the connected socket
  */
-void i_handler(char *i_value, int *input_fd) {
-    i_value += 4;  // skip the "TCPS" prefix
-    int port = atoi(i_value);
-
+int open_tcp_server_and_accept(int port) {
     // create TCP socket that will listen to input on localhost:port
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1) {
         perror("error creating socket");
-        exit(EXIT_FAILURE);
+        cleanup_and_exit(EXIT_FAILURE);
     }
 
     // allow the socket to be reused
     int optval = 1;
     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) == -1) {
         perror("setsockopt");
-        exit(EXIT_FAILURE);
+        cleanup_and_exit(EXIT_FAILURE);
     }
 
     // bind the socket to the address
@@ -89,49 +53,34 @@ void i_handler(char *i_value, int *input_fd) {
 
     if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
         perror("error binding socket");
-        exit(EXIT_FAILURE);
+        cleanup_and_exit(EXIT_FAILURE);
     }
 
     // listen for incoming connections - at most 1
     if (listen(sockfd, 1) == -1) {
         perror("error listening on socket");
-        exit(EXIT_FAILURE);
+        cleanup_and_exit(EXIT_FAILURE);
     }
 
     // accept the connection and change the input_fd to the new socket
     struct sockaddr_in client_addr;
     socklen_t client_addr_len = sizeof(client_addr);
-    *input_fd = accept(sockfd, (struct sockaddr *)&client_addr, &client_addr_len);
+    int client_fd = accept(sockfd, (struct sockaddr *)&client_addr, &client_addr_len);
 
-    if (*input_fd == -1) {
+    if (client_fd == -1) {
         perror("error accepting connection");
-        exit(EXIT_FAILURE);
+        cleanup_and_exit(EXIT_FAILURE);
     }
+    return client_fd;
 }
 
 /**
- * Open a TCP client to the given server
- * @param o_value the server IP and port (in format "TCPC<server_ip>,<server_port>")
- * @param input_fd the current input file descriptor (so that it can be closed if needed)
- * @param output_fd the file descriptor to store the new socket (return value)
+ * Connect to a TCP server
+ * @param server_addr the server IP or hostname
+ * @param server_port the server port
+ * @param input_fd the file descriptor of the connection socket
  */
-void o_handler(char *o_value, int input_fd, int *output_fd) {
-    o_value += 4;  // skip the "TCPC" prefix
-
-    // split the string to get the server IP/hostname and port
-    char *server_ip = strtok(o_value, ",");
-    if (server_ip == NULL) {
-        fprintf(stderr, "Invalid server IP/hostname\n");
-        exit(EXIT_FAILURE);
-    }
-
-    // get the rest of the string after the comma
-    char *server_port = strtok(NULL, ",");
-    if (server_port == NULL) {
-        fprintf(stderr, "Invalid server port\n");
-        exit(EXIT_FAILURE);
-    }
-
+int connect_to_tcp_server(char *server_addr, char *server_port) {
     // get address info
     struct addrinfo hints, *res, *p;
     int status;
@@ -142,9 +91,9 @@ void o_handler(char *o_value, int input_fd, int *output_fd) {
     hints.ai_socktype = SOCK_STREAM;
 
     // get address info
-    if ((status = getaddrinfo(server_ip, server_port, &hints, &res)) != 0) {
+    if ((status = getaddrinfo(server_addr, server_port, &hints, &res)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
-        exit(EXIT_FAILURE);
+        cleanup_and_exit(EXIT_FAILURE);
     }
 
     // loop through the results and connect to the first we can
@@ -165,76 +114,123 @@ void o_handler(char *o_value, int input_fd, int *output_fd) {
 
     if (p == NULL) {
         fprintf(stderr, "failed to connect\n");
-        if (input_fd != STDIN_FILENO) {
-            close(input_fd);
-        }
-        exit(EXIT_FAILURE);
+        cleanup_and_exit(EXIT_FAILURE);
     }
 
     freeaddrinfo(res);  // free the linked list
 
-    *output_fd = sockfd;
+    return sockfd;
 }
+
 /**
- * Open a TCP server to listen to the given port. The input and output file descriptors will be the same in the end of the function
- * @param b_value the port to listen to (in format "TCPS<port>")
- * @param input_fd the file descriptor to store the new socket (return value)
- * @param output_fd the file descriptor to store the new socket (return value)
- *
+ * Parse the hostname and port from the given string
+ * @param value the string to parse in the format "<hostname>,<port>"
+ * @param hostname the pointer to store the hostname (return value)
+ * @param port the pointer to store the port (return value)
  */
-void b_handler(char *b_value, int *input_fd, int *output_fd) {
-    // open TCP server to listen to the port
-    b_value += 4;  // skip the "TCPS" prefix
-    int port = atoi(b_value);
-
-    // create TCP socket that will listen to input on localhost:port
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
-        perror("error creating socket");
-        exit(EXIT_FAILURE);
+void parse_hostname_port(char *value, char **hostname, char **port) {
+    // split the string to get the server IP/hostname and port
+    *hostname = strtok(value, ",");
+    if (*hostname == NULL) {
+        fprintf(stderr, "Invalid server IP/hostname\n");
+        cleanup_and_exit(EXIT_FAILURE);
     }
 
-    // allow the socket to be reused
-    int optval = 1;
-    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) == -1) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
+    // get the rest of the string after the comma
+    *port = strtok(NULL, ",");
+    if (*port == NULL) {
+        fprintf(stderr, "Invalid server port\n");
+        cleanup_and_exit(EXIT_FAILURE);
+    }
+}
+
+/**
+ * Run the program with the given arguments
+ */
+void run_program(char *args_as_string) {
+    // tokenize the string - split by space
+    char *token = strtok(args_as_string, " ");
+
+    if (token == NULL) {
+        fprintf(stderr, "No arguments provided\n");
+        cleanup_and_exit(EXIT_FAILURE);
+    }
+    // create an array of strings to store the arguments
+    char **args = (char **)malloc(sizeof(char *));
+    int n = 0;          // number of arguments
+    args[n++] = token;  // add the first argument
+
+    // get the rest of the arguments
+    while (token != NULL) {
+        token = strtok(NULL, " ");                                    // get the next token (NULL - take the next token from the previous string)
+        char **t = (char **)realloc(args, (n + 1) * sizeof(char *));  // allocate memory for the new argument
+        // check if the memory allocation failed
+        if (t == NULL) {
+            fprintf(stderr, "Memory allocation failed\n");
+            free(args);
+            cleanup_and_exit(EXIT_FAILURE);
+        }
+        args = t;
+        args[n++] = token;  // add the new argument and increment the number of arguments
+    }
+    // fork and execute the program
+    int fd = fork();
+    if (fd < 0) {  // fork failed
+        fprintf(stderr, "Fork failed\n");
+        cleanup_and_exit(EXIT_FAILURE);
     }
 
-    // bind the socket to the address
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    if (fd == 0) {  // child process
+        execvp(args[0], args);
+        fprintf(stderr, "Exec failed\n");
+        free(args);
+        cleanup_and_exit(EXIT_FAILURE);
+    } else {
+        wait(NULL);  // wait for the child process to finish
+        // free the memory
+        free(args);
+        fflush(stdout);
+    }
+}
 
-    if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
-        perror("error binding socket");
-        exit(EXIT_FAILURE);
+/**
+ * Update the input and output file descriptors
+ * @param value the value to update the file descriptors
+ * @param input_need_change 1 if the input file descriptor needs to be changed, 0 otherwise
+ * @param output_need_change 1 if the output file descriptor needs to be changed, 0 otherwise
+ */
+void input_output_updater(char *value, int input_need_change, int output_need_change) {
+    int new_fd;
+    if (strncmp(value, "TCPS", 4) == 0) {
+        // open TCP server to listen to the port
+        value += 4;  // skip the "TCPS" prefix
+        int port = atoi(value);
+        new_fd = open_tcp_server_and_accept(port);
+
+    } else if (strncmp(value, "TCPC", 4) == 0) {
+        // open TCP client to connect to the server
+        value += 4;  // skip the "TCPC" prefix
+        char *server_ip, *server_port;
+        parse_hostname_port(value, &server_ip, &server_port);
+        new_fd = connect_to_tcp_server(server_ip, server_port);
+    } else {
+        printf("Invalid input/output value! need to start with TCPS or TCPC\n");
+        cleanup_and_exit(EXIT_FAILURE);
     }
 
-    // listen for incoming connections - at most 1
-    if (listen(sockfd, 1) == -1) {
-        perror("error listening on socket");
-        exit(EXIT_FAILURE);
+    if (input_need_change) {
+        input_fd = new_fd;
     }
-
-    // accept the connection and change the input_fd & output_fd to the new socket
-    struct sockaddr_in client_addr;
-    socklen_t client_addr_len = sizeof(client_addr);
-    int new_fd = accept(sockfd, (struct sockaddr *)&client_addr, &client_addr_len);
-    if (new_fd == -1) {
-        perror("error accepting connection");
-        exit(EXIT_FAILURE);
+    if (output_need_change) {
+        output_fd = new_fd;
     }
-
-    *input_fd = new_fd;
-    *output_fd = new_fd;
 }
 
 int main(int argc, char *argv[]) {
+    char *usage_msg = "Usage: %s -e <value> [-b <value>] [-i <value>] [-o <value>] \n";
     if (argc < 2) {
-        fprintf(stderr, "Usage: %s -e <value> [-b <value>] [-i <value>] [-o <value>]\n", argv[0]);
-        exit(EXIT_FAILURE);
+        fprintf(stderr, usage_msg, argv[0]);
+        cleanup_and_exit(EXIT_FAILURE);
     }
 
     // parse the arguments
@@ -259,67 +255,63 @@ int main(int argc, char *argv[]) {
                 o_value = optarg;
                 break;
             default:
-                fprintf(stderr, "Usage: %s -e <value> [-b <value>] [-i <value>] [-o <value>]\n", argv[0]);
-                exit(EXIT_FAILURE);
+                fprintf(stderr, usage_msg, argv[0]);
+                cleanup_and_exit(EXIT_FAILURE);
         }
-    }
-
-    // check if we got the -e option (if not, print an error message and exit the program)
-    if (e_value == NULL) {
-        fprintf(stderr, "Option -e is required\n");
-        fprintf(stderr, "Usage: %s -e <value> [-b <value>] [-i <value>] [-o <value>]\n", argv[0]);
-        exit(EXIT_FAILURE);
     }
 
     // check if -b is used with -i or -o (if so, print an error message and exit the program)
     if (b_value != NULL && (i_value != NULL || o_value != NULL)) {
-        fprintf(stderr, "Option -b cannot be used with -i or -o\n");
-        fprintf(stderr, "Usage: %s -e <value> [-b <value>] [-i <value>] [-o <value>]\n", argv[0]);
-        exit(EXIT_FAILURE);
+        fprintf(stderr, "Error: Option -b cannot be used with -i or -o\n");
+        cleanup_and_exit(EXIT_FAILURE);
     }
 
-    // start the input and output file descriptors with the default values
-    // and will change them if needed
-    int input_fd = STDIN_FILENO;
-    int output_fd = STDOUT_FILENO;
+    if (b_value == NULL && i_value == NULL && o_value == NULL) {
+        fprintf(stderr, "Error: At least one of -b, -i or -o must be provided\n");
+        cleanup_and_exit(EXIT_FAILURE);
+    }
 
     if (i_value != NULL) {
-        i_handler(i_value, &input_fd);
+        input_output_updater(i_value, 1, 0);
     }
 
     if (o_value != NULL) {
-        o_handler(o_value, input_fd, &output_fd);
+        input_output_updater(o_value, 0, 1);
     }
 
     if (b_value != NULL) {
-        b_handler(b_value, &input_fd, &output_fd);
+        input_output_updater(b_value, 1, 1);
     }
 
-    // redirect the input and output to the new file descriptors
+    if (e_value != NULL) {
+        // redirect the input and output to the new file descriptors
+        if (input_fd != STDIN_FILENO) {
+            if (dup2(input_fd, STDIN_FILENO) == -1) {
+                perror("dup2 input");
+                cleanup_and_exit(EXIT_FAILURE);
+            }
+        }
+
+        if (output_fd != STDOUT_FILENO) {
+            if (dup2(output_fd, STDOUT_FILENO) == -1) {
+                perror("dup2 output");
+                cleanup_and_exit(EXIT_FAILURE);
+            }
+        }
+
+        // run the program with the given arguments
+        run_program(e_value);
+    } else {
+        printf("YOU DIDN'T PROVIDE -e!!!! BYEEEE\n");
+    }
+
+    // close the file descriptors
     if (input_fd != STDIN_FILENO) {
-        if (dup2(input_fd, STDIN_FILENO) == -1) {
-            close(input_fd);
-            if (output_fd != STDOUT_FILENO) {
-                close(output_fd);
-            }
-            perror("dup2 input");
-            exit(EXIT_FAILURE);
-        }
+        close(input_fd);
     }
 
-    if (output_fd != STDOUT_FILENO) {
-        if (dup2(output_fd, STDOUT_FILENO) == -1) {
-            close(output_fd);
-            if (input_fd != STDIN_FILENO) {
-                close(input_fd);
-            }
-            perror("dup2 output");
-            exit(EXIT_FAILURE);
-        }
+    if (output_fd != STDOUT_FILENO && output_fd != input_fd) {
+        close(output_fd);
     }
-
-    // run the program with the given arguments
-    run_program(e_value);
-    // TODO: check how to close the sockets
     return 0;
 }
