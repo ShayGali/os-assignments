e491d5d4909ed50c8c80b32984e77858744d1417
shayg <shaygali10@gmail.com>
2024-06-26 17:35:03 +0300

    update

--- ex3/q5/reactor.cpp
+++ ex3/q5/reactor.cpp
@@ -10,7 +10,7 @@ constexpr int MAX_CLIENT = 10;
 
 reactor::reactor() {
     running = false;
-    fd_to_func = std::map<int, reactorFunc>();
+    fd_to_func = map<int, reactorFunc>();
 }
 
 int reactor::add_fd_to_reactor(int fd, reactorFunc func) {
@@ -31,23 +31,27 @@ int reactor::start() {
     if (running) {
         return -1;
     }
+
     running = true;
     std::thread t1([this] {
         fd_set read_fds;     // temp file descriptor list for select()
         char buf[BUF_SIZE];  // buffer for client data
         int nbytes;
+
+        // start the reactor
         while (running) {
             // init the fdmax
             int fdmax;
             if (fd_to_func.empty()) {
                 fdmax = 0;
             } else {
-                fdmax = fd_to_func.rbegin()->first;
+                fdmax = fd_to_func.rbegin()->first; // Get the last element of the map (rbegin() returns reverse iterator pointing to last element of map), and then take the fd of that element
             }
 
             // init the read_fds
             FD_ZERO(&read_fds);
 
+            // add the file descriptors to read_fds
             for (const auto &pair : fd_to_func) {
                 FD_SET(pair.first, &read_fds);  // Add each file descriptor to fd_set
             }
@@ -55,7 +59,7 @@ int reactor::start() {
             // run through the existing connections looking for data to read
             for (int i = 0; i <= fdmax; i++) {
                 if (FD_ISSET(i, &read_fds)) {
-                    fd_to_func[i](i);
+                    fd_to_func[i](i); // Call the function associated with the file descriptor, and pass the file descriptor as argument
                 }
             }
         }
