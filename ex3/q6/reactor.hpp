006cd38b5038ad58d1d2a269a294e8275c968e26
shayg <shaygali10@gmail.com>
2024-06-27 10:12:58 +0300

    update

--- ex3/q6/reactor.hpp
+++ ex3/q6/reactor.hpp
@@ -1,1 +1,25 @@
-../q5/reactor.hpp
+#include <functional>
+#include <map>
+#include <thread>
+
+using std::function;
+using std::map;
+using std::thread;
+
+typedef function<void*(int)> reactorFunc;
+
+class reactor {
+   private:
+    bool running;
+    map<int, reactorFunc> fd_to_func;
+    thread eventLoopThread;
+
+   public:
+    reactor();
+    ~reactor();
+    int add_fd_to_reactor(int fd, reactorFunc func);
+    int remove_fd_from_reactor(int fd);
+
+    int start();
+    int stop();
+};
